930dec133bf8ca136fb0b5087850ecd0fe115f55
Lenkaa3792 <allanlenkaa@gmail.com>
2022-06-15 14:15:20 +0300

    commit tree 06db5e38b2415371b9c7312b6d456215c3db7cca

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function addingEventListener ()
-{
-    const sinon = document.getElementById( "input" )
-    function addingEventListener ()
-    {
-        sinon.addEventListener( "click", function ( event )
-        {
-            return "clicked"
-        } )
-    }
+const input = document.querySelector("input");
+
+function addingEventListener() {
+  input.addEventListener("click", function (event) {
+    return "clicked";
+  });
 }
